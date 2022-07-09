67804dccd96dd0abc5273bff90eeeb7818594ebb
Abheek Dhawan <abheekd@protonmail.com>
2022-07-08 20:59:57 -0500

    feat: large update
    - dark mode
    - scroll to elements instead of pages
    - add more bios

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,41 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 // components
-import Navbar from '../components/navbar';
-import Footer from './footer';
+import Navbar from '../components/navbar'
+import Footer from './footer'
 
 // styles
-import cx from 'classnames';
-import Head from 'next/head';
+import cx from 'classnames'
+import Head from 'next/head'
+import { ChevronDoubleUpIcon } from '@heroicons/react/outline'
 
-export default function Layout({ children, active } : { children: React.ReactNode, active: string }) {
-    const [isDarkMode, setDarkMode] = useState(true);
+export default function Layout({
+    children,
+    active,
+}: {
+    children: React.ReactNode
+    active: string
+}) {
+    const [isDarkMode, setDarkMode] = useState(true)
+    const [scrollTop, setScrollTop] = useState(0)
 
     const toggleDarkMode = () => {
-        setDarkMode(!isDarkMode);
-        window.localStorage.setItem('isDarkMode', `${isDarkMode}`);
+        setDarkMode(!isDarkMode)
+        window.localStorage.setItem('isDarkMode', `${isDarkMode}`)
     }
 
     useEffect(() => {
-        if ((window.localStorage.getItem('isDarkMode') === 'true') || (window.localStorage.getItem('isDarkMode') === null && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            setDarkMode(false);
+        if (
+            window.localStorage.getItem('isDarkMode') === 'true' ||
+            (window.localStorage.getItem('isDarkMode') === null &&
+                window.matchMedia('(prefers-color-scheme: dark)').matches)
+        ) {
+            setDarkMode(false)
         } else {
-            setDarkMode(true);
+            setDarkMode(true)
         }
-    }, [isDarkMode]);
+    }, [isDarkMode])
+
+    useEffect(() => {
+        function onScroll() {
+            let currentPosition = window.pageYOffset
+            setScrollTop(currentPosition <= 0 ? 0 : currentPosition)
+        }
+
+        window.addEventListener('scroll', onScroll)
+        return () => window.removeEventListener('scroll', onScroll)
+    }, [scrollTop]);
 
     return (
         <>
             <Head>
-                <title>{active.toLowerCase().split(' ').map(w => w.charAt(0).toUpperCase() + w.slice(1))} | CASTL FLL</title>
+                <title>
+                    {active
+                        .toLowerCase()
+                        .split(' ')
+                        .map(
+                            (w) => w.charAt(0).toUpperCase() + w.slice(1)
+                        ) + ' | CASTL FLL'}
+                </title>
             </Head>
-            <div className={cx({dark: isDarkMode}, 'transition-colors')}>
-                <div className={cx("bg-slate-50 dark:bg-slate-700 h-full", { dark: isDarkMode })}>
-                    <Navbar active={active} isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-                    <main className={cx({dark: isDarkMode})}>{children}</main>
+            <div className={cx({ dark: isDarkMode }, 'transition-colors')}>
+                <div
+                    className={cx('bg-slate-50 dark:bg-slate-700 h-full', {
+                        dark: isDarkMode,
+                    })}
+                >
+                    <Navbar
+                        active={active}
+                        isDarkMode={isDarkMode}
+                        toggleDarkMode={toggleDarkMode}
+                    />
+                    <main className={cx({ dark: isDarkMode })}>{children}</main>
+                    <div className='fixed bottom-8 right-8'><a href='#' className={cx({ 'pointer-events-none': scrollTop === 0 })}><ChevronDoubleUpIcon className={cx('h-10 w-10 opacity-100 transition-all delay-200', { 'opacity-0': scrollTop === 0 })} /></a></div>
                     <Footer />
                 </div>
             </div>
         </>
-    );
+    )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -29,27 +29,24 @@ export default function Navbar({ active, isDarkMode, toggleDarkMode } : { active
                     <div className='md:flex md:justify-between'>
                     <ul className={cx("flex flex-col ease-in-out duration-500 mt-4 mx-6 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium")}>
                         <li>
-                            <Link href='/'>
+                            <Link href='#'>
                                 <a className={cx(styles.navLink, { 'bg-blue-700 md:bg-transparent text-slate-50 md:text-blue-600 dark:text-white': active === 'home', 'text-gray-700 border-gray-100 hover:bg-gray-50 md:hover:bg-transparent dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:hover:text-white dark:border-gray-700': active !== 'home' })}>Home</a>
                             </Link>
                         </li>
                         <li>
-                            <Link href='/about'>
+                            <Link href='#about'>
                                 <a className={cx(styles.navLink, { 'bg-blue-700 md:bg-transparent text-slate-50 md:text-blue-600 dark:text-white': active === 'about', 'text-gray-700 border-gray-100 hover:bg-gray-50 md:hover:bg-transparent dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:hover:text-white dark:border-gray-700': active !== 'about' })}>About</a>
                             </Link>
                         </li>
                         <li>
-                            <Link href='/contact'>
+                            <Link href='#contact'>
                                 <a className={cx(styles.navLink, { 'bg-blue-700 md:bg-transparent text-slate-50 md:text-blue-600 dark:text-white': active === 'contact', 'text-gray-700 border-gray-100 hover:bg-gray-50 md:hover:bg-transparent dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:hover:text-white dark:border-gray-700': active !== 'contact' })}>Contact</a>
                             </Link>
                         </li>
                     </ul>
                     <ul className={cx("flex flex-wrap space-x-3 mt-4 mx-6 md:flex-row md:space-x-3 md:mt-0 md:text-sm md:font-medium")}>
                         <li>
-                            <button className={cx(styles.navButton, styles.blue)}>test</button>
-                        </li>
-                        <li>
-                            <button className={cx(styles.navButton, styles.blue)}>test</button>
+                            <a href='#learn-more'><button className={cx(styles.navButton, styles.blue)}>Learn more</button></a>
                         </li>
                         <li>
                             <button type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={toggleDarkMode}>
